3d7bb01c4a90cea322315d4f417212678f6f0727
PrahaldChouhan <prahladchouhan928@gmail.com>
2025-04-17 12:29:27 +0530

    day-46 tasks completed

--- Day-46(event bubbling)/eventBubbling/script.js
+++ Day-46(event bubbling)/eventBubbling/script.js
@@ -4,35 +4,56 @@ var arr=[
         age:23,
         married:false,
         istatus:'stranger',
-        image:"https://images.unsplash.com/photo-1727798448236-f99a244215e0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aW1hZ2UlMjBtYWxlJTIwc3xlbnwwfHwwfHx8MA%3D%3D"
+        image:"https://images.unsplash.com/photo-1566753323558-f4e0952af115?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFsZXxlbnwwfHwwfHx8MA%3D%3D"
     },
     {
         username:'sumit',
         age:45,
         married:true,
         istatus:'stranger',
-        image:"https://images.unsplash.com/photo-1723201223411-7a53a744055c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aW1hZ2UlMjBtYWxlJTIwc3xlbnwwfHwwfHx8MA%3D%3D"
+        image:"https://images.unsplash.com/photo-1524666041070-9d87656c25bb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbGV8ZW58MHx8MHx8fDA%3D"
     },
     {
         username:'rohan',
         age:50,
         married:true,
         istatus:'stranger',
-        image:"https://images.unsplash.com/photo-1654417202064-b0484e3cc815?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGltYWdlJTIwbWFsZSUyMHN8ZW58MHx8MHx8fDA%3D"
+        image:"https://images.unsplash.com/photo-1541577141970-eebc83ebe30e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hbGV8ZW58MHx8MHx8fDA%3D"
     }
 ];
 
 var main=document.querySelector(".main")
-var sum=''
-arr.forEach(function(elem)
+function herofunc()
+{
+    var sum=''
+arr.forEach(function(elem,idx)
 {
     sum=sum+`<div class="card">
             <img src="${elem.image}"/>
             <h2>${elem.username} ,${elem.age}</h2>
             <h4>Married:${elem.married}</h4>
             <h3>${elem.istatus}</h3>
-            <button>Add Friend</button>
+            <button id=${idx}>Add Friend</button>
         </div>`;
 })
 
 main.innerHTML=sum;
+
+}
+herofunc();
+main.addEventListener('click',function(detail){
+    var goldvalue=arr[detail.target.id];
+
+    if(goldvalue.istatus=='stranger')
+    {
+        goldvalue.istatus='Freinds';
+    }
+    else{
+        goldvalue.istatus='stranger';
+
+    }
+    herofunc();
+  
+
+
+})
